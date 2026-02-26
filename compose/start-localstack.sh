1da2d2417d53ec5e7c3def40c7a80f6d8d1387df
David Shepherd <6839214+shepda@users.noreply.github.com>
2026-02-26 12:38:24 +0000

    DWT-1493 Update docker compose with Waste Organisation Backend (#14)

---
+++ compose/start-localstack.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export AWS_REGION=eu-west-2
+export AWS_DEFAULT_REGION=eu-west-2
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+
+# waste receiver backend processing queue
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name waste-receiver-background-process
+
+### Copied from cdp-uploader/compose/01-start-localstack.sh
+echo "[INIT SCRIPT] Creating buckets" >&2
+
+aws --endpoint-url=http://localhost:4566 s3 mb s3://cdp-uploader-quarantine
+aws --endpoint-url=http://localhost:4566 s3 mb s3://my-bucket
+
+echo "[INIT SCRIPT] Creating queues" >&2
+
+# queues
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-clamav-results
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-uploader-scan-results-callback.fifo --attributes "{\"FifoQueue\":\"true\",\"ContentBasedDeduplication\": \"true\"}"
+
+# test harness
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name mock-clamav
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration --bucket cdp-uploader-quarantine --notification-configuration '{"QueueConfigurations": [{"QueueArn": "arn:aws:sqs:eu-west-2:000000000000:mock-clamav","Events": ["s3:ObjectCreated:*"]}]}'
+aws --endpoint-url=http://localhost:4566 sqs create-queue --region $AWS_REGION --queue-name cdp-uploader-download-requests
+
+echo READY > /tmp/READY
+### End of copied section
