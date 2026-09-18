f1ae08c12154b7a9f0b16f5418c1e0a708bb3c80
David Shepherd <david@x-designs.co.uk>
2026-09-18 14:59:57 +0100

    DWTA-380 Update waste-movement-utils to use npm version in package.json

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ export default {
       '@defra/hapi-tracing', // Supports ESM only
       'node-fetch', // Supports ESM only
       'uuid',
-      'waste-movement-utils'
+      '@defra/waste-movement-utils'
     ].join('|')}/)`
   ]
 }
